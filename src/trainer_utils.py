519eb2269926376bbf63eb40c2e0fdb4ca7587d2
An Binh <nguyenanbinh96@gmail.com>
2022-03-30 19:55:01 +0700

    init

---
+++ src/trainer_utils.py
@@ -0,0 +1,64 @@
+import os
+from typing import Optional
+
+import torch
+from torch import nn
+from transformers import Trainer, AdapterTrainer, PreTrainedModel
+from transformers.trainer import unwrap_model
+
+
+class CustomTrainer(Trainer):
+    def compute_loss(self, model, inputs, return_outputs=False):
+        labels = inputs.get("labels")
+        # forward pass
+        outputs = model(**inputs)
+        logits = outputs.get("logits")
+        # compute custom loss (suppose one has 3 labels with different weights)
+        loss_fct = nn.CrossEntropyLoss(ignore_index=-100)
+        loss = loss_fct(logits.view(-1, self.model.config.num_labels), labels.view(-1))
+        return (loss, outputs) if return_outputs else loss
+    
+    
+    def _save(self, output_dir: Optional[str] = None, state_dict=None):
+        # If we are executing this function, we are the process zero, so we don't check for that.
+        output_dir = output_dir if output_dir is not None else self.args.output_dir
+        os.makedirs(output_dir, exist_ok=True)
+        print(f"Saving model checkpoint to {output_dir}")
+        # Save a trained model and configuration using `save_pretrained()`.
+        # They can then be reloaded using `from_pretrained()`
+        if not isinstance(self.model, PreTrainedModel):
+            if isinstance(unwrap_model(self.model), PreTrainedModel):
+                if state_dict is None:
+                    state_dict = self.model.state_dict()
+                unwrap_model(self.model).save_pretrained(output_dir, state_dict=state_dict)
+            else:
+                # logger.info("Trainer.model is not a `PreTrainedModel`, only saving its state dict.")
+                if state_dict is None:
+                    state_dict = self.model.state_dict()
+                torch.save(state_dict, os.path.join(output_dir, "pytorch_model.bin"))
+        else:            
+            self.model.config.to_json_file(os.path.join(output_dir, 'config.json'))
+            if self.model.use_adapter:
+                self.model.roberta.save_all_adapters(output_dir)
+                if hasattr(self.model, "classifier"):
+                    torch.save(self.model.classifier.state_dict(), os.path.join(output_dir, "classifier.bin"))
+            else:
+                self.model.save_pretrained(output_dir, state_dict=state_dict)
+                
+        if self.tokenizer is not None:
+            self.tokenizer.save_pretrained(output_dir)
+
+        # Good practice: save your training arguments together with the trained model
+        torch.save(self.args, os.path.join(output_dir, "training_args.bin"))
+
+
+class CustomAdapterTrainer(AdapterTrainer):
+    def compute_loss(self, model, inputs, return_outputs=False):
+        labels = inputs.get("labels")
+        # forward pass
+        outputs = model(**inputs)
+        logits = outputs.get("logits")
+        # compute custom loss (suppose one has 3 labels with different weights)
+        loss_fct = nn.CrossEntropyLoss(ignore_index=-100)
+        loss = loss_fct(logits.view(-1, self.model.config.num_labels), labels.view(-1))
+        return (loss, outputs) if return_outputs else loss
